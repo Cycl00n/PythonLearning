c79038af835120ddd48b190cd78edf59b572ec7f
Cycl00n <nicolas-clement1997@hotmail.fr>
2025-11-27 10:30:20 +0100

    corrected first ex

--- Python 0 - Starting/ex00/Hello.py
+++ Python 0 - Starting/ex00/Hello.py
@@ -4,9 +4,11 @@ ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello": "titi!"}
 
 ft_list[1] = "World!"
-ft_tuple = ("Hello", "Luxembourg!")
-ft_set = {"Hello", "Luxembourg!"}
-ft_dict = {"Hello!", "42Luxembourg!"}
+ft_tuple = (ft_tuple[0], "Luxembourg!")
+ft_set.remove("tutu!")
+ft_set.add("Luxembourg!")
+ft_dict["Hello"] = "42Luxembourg!"
+
 
 print(ft_list)
 print(ft_tuple)
